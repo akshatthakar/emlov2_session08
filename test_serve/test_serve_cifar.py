6c5bcce496437b00187024bbd9097babd9e0440a
akshatthakar <akshat.thakar@gmail.com>
2022-11-22 01:09:49 +0530

    Update test_serve_cifar.py

--- test_serve/test_serve_cifar.py
+++ test_serve/test_serve_cifar.py
@@ -7,24 +7,16 @@ import torch
 
 class TestServe(unittest.TestCase):
 
-   @classmethod
-   def setUpClass(cls):
-        cls.host_name = "54.64.217.123"
-        print(f"using host_name={cls.host_name}\n\n")
-        cls.image_paths = ["cat.png", "ship.png", "automobile.png", "dog.png","airplane.png","bird.png","deer.png","frog.png","horse.png","truck.png"]
-
    @pytest
-   def test_cifar_inference(self):
-        for image_path in self.image_paths:
+   def test_cifar_inference(host_name):
+        image_paths = ["cat.png", "ship.png", "automobile.png", "dog.png","airplane.png","bird.png","deer.png","frog.png","horse.png","truck.png"]
+        for image_path in image_paths:
             complete_path = "images/" + image_path
-            response: Response = requests.post(f"http://{self.host_name}:8080/predictions/cifar/1.0", files={'data': open(complete_path, 'rb')})
+            response: Response = requests.post(f"http://{host_name}:8080/predictions/cifar/1.0", files={'data': open(complete_path, 'rb')})
             response_json = res.json()
-            data = response.json()['data'][0]
-            predicted_label = data['label']
+            predicted_label = list(response_json.keys())[0]
             print(f"predicted label: {predicted_label}")
-
             self.assertEqual(image_path.split(".")[0], predicted_label)
-
             print(f"done testing: {image_path}")
             
 if __name__ == '__main__':
